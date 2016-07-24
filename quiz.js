69a49edaf005487b02b6d156de6a4ce914114242
Thomas J. Shavor <thomasshavor@gmail.com>
2016-07-24 08:37:24 -0500

    tree grows

---
+++ quiz.js
@@ -0,0 +1,65 @@
+
+////////////EVENT LISTENERS HERE:///////////////////////////////////
+
+var outputEl = document.getElementById("submitButton").addEventListener("click", function (event){
+	(tree);
+// console.log ("event", event);
+});
+
+
+
+///////ENTER BUTTON EVENT LISTENER//////////
+function inputKeyUp(event) {
+
+    if(event.keyCode === 13) {
+    	(tree);
+    }
+    console.log ("ENTER key used!")
+  };
+
+/////FUNCTION THAT CALCULATES VALUES FOR CHARACTERS AND SPACES//////////////
+
+
+function tree (clickEvent){
+
+	var enteredUserHeight = document.getElementById('inputHeight').value;
+	pineTree.treeCharacter = document.getElementById('inputChar').value;
+
+
+// this needs to correspond to the DOM element in the input box
+// var enteredUserHeight = 5;
+	for (var i = 0; i< enteredUserHeight; i++) {
+
+		 var spaces= enteredUserHeight-i;
+		 var newSpaces= " ".repeat(spaces);
+
+
+	   var char= ((2*i)+1);
+// this needs to correspond to the DOM element in the input box
+		 var newChar= "&".repeat(char);
+
+
+		 var makeTree= newSpaces + newChar;
+		 console.log (makeTree);
+	}
+};
+trees();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
