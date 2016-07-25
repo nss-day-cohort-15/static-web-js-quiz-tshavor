0c7bf26365229123df6bdb90d6bbd51609361cb1
Thomas J. Shavor <thomasshavor@gmail.com>
2016-07-25 08:26:09 -0500

    tree grows

--- quiz.js
+++ quiz.js
@@ -1,21 +1,21 @@
 
 ////////////EVENT LISTENERS HERE:///////////////////////////////////
 
-var outputEl = document.getElementById("submitButton").addEventListener("click", function (event){
-	(tree);
+// var outputEl = document.getElementById("submitButton").addEventListener("click", function (event){
+// 	(growtree);
 // console.log ("event", event);
-});
+// });
 
 
 
 ///////ENTER BUTTON EVENT LISTENER//////////
-function inputKeyUp(event) {
+// function inputKeyUp(event) {
 
-    if(event.keyCode === 13) {
-    	(tree);
-    }
-    console.log ("ENTER key used!")
-  };
+//     if(event.keyCode === 13) {
+//     	(growtree);
+//     }
+//     console.log ("ENTER key used!")
+//   };
 
 /////FUNCTION THAT CALCULATES VALUES FOR CHARACTERS AND SPACES//////////////
 
@@ -23,27 +23,26 @@ function inputKeyUp(event) {
 function tree (clickEvent){
 
 	var enteredUserHeight = document.getElementById('inputHeight').value;
-	pineTree.treeCharacter = document.getElementById('inputChar').value;
+	var inputChar = document.getElementById('inputChar').value;
+
 
 
 // this needs to correspond to the DOM element in the input box
-// var enteredUserHeight = 5;
-	for (var i = 0; i< enteredUserHeight; i++) {
+     // var enteredUserHeight = 5;
+	   for (var i = 0; i< enteredUserHeight; i++) {
 
 		 var spaces= enteredUserHeight-i;
 		 var newSpaces= " ".repeat(spaces);
-
-
 	   var char= ((2*i)+1);
 // this needs to correspond to the DOM element in the input box
 		 var newChar= "&".repeat(char);
-
+		 // var newChar= inputChar.repeat(char);
 
 		 var makeTree= newSpaces + newChar;
 		 console.log (makeTree);
 	}
 };
-trees();
+tree();
 
 
 
